8700d39e716587a2e2a8b959861ae456179d7f59
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2015-05-28 11:53:19 -0400

    forgot to eliminate iphone5 landscape

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ require([
         if( uagent.search( "iphone" ) > -1 ) iphone = true;
         if( uagent.search( "android" ) > -1 ) android = true;
 
-        if( iphone && (window.screen.height == 480 || window.screen.height == 320) )
+        if( iphone && ( (window.screen.height == 480 && window.screen.width == 320) || (window.screen.width == 480 && window.screen.height == 320) ) )
             iphone34 = true;
 
         /*-------- set body tags ---------*/
